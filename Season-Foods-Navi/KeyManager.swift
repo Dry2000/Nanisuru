337a809e584d631e1559f1956e175a9846440c87
Dry <e1625@s.akashi.ac.jp>
2019-12-28 21:34:53 +0900

    Add:apikey

---
+++ Season-Foods-Navi/KeyManager.swift
@@ -0,0 +1,16 @@
+//
+//  KeyManager.swift
+//  Season-Foods-Navi
+//
+//  Created by 洞井僚太 on 2019/12/14.
+//  Copyright © 2019 洞井僚太. All rights reserved.
+//
+
+import Foundation
+class Key{
+    var key:Int!
+    init(){
+        self.key = 1077779649169295378
+    }
+}
+
